1bbf69430f80ab006436a67614264842f66d7a5b
Afranioalves <afranio777alves@gmail.com>
2024-04-28 18:05:55 +0100

    style: add icon and animation lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '../styles/boxicons-2.1.4/css/boxicons.css'
+import '@/styles/aos-css/aos.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
