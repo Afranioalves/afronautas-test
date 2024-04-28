a214c58075401101e5f76a35f23877a1f81e8e48
Afranioalves <afranio777alves@gmail.com>
2024-04-28 18:08:16 +0100

    chore: add curses array

---
+++ src/data/curses.ts
@@ -0,0 +1,125 @@
+
+
+export const curses = [
+    {
+        id: 1,
+        curse_name: "Desenvolvimento Web",
+        description: "Aprenda a criar aplicativos web dinâmicos utilizando tecnologias como HTML, CSS e JavaScript.",
+        localization: "Online",
+        student_number: "12,356.868",
+        cover:'/img/security.jpg',
+        type:'HTML+CSS+JAVASCRIPT',
+        mark:'Design'
+    },
+    {
+        id: 2,
+        curse_name: "Desenvolvimento de Jogos",
+        description: "Explore o mundo da criação de jogos, desde o design de personagens até a programação de mecânicas de jogo.",
+        localization: "Presencial",
+        student_number: "56.868",
+        cover:'/img/game.jpg',
+        type:"C# + Unity3D",
+        mark:'Design'
+    },
+    {
+        id: 3,
+        curse_name: "Ciência de Dados",
+        description: "Aprenda a extrair insights valiosos de grandes conjuntos de dados usando Python e técnicas avançadas de análise.",
+        localization: "Online",
+        student_number: "356.868",
+        cover:'/img/data.jpeg',
+        type:"Python + OpenCv",
+        mark:'Coding'
+    },
+    {
+        id: 4,
+        curse_name: "Desenvolvimento Mobile",
+        description: "Domine o desenvolvimento de aplicativos para dispositivos móveis iOS e Android usando frameworks populares como React Native.",
+        localization: "Presencial",
+        student_number: "1,356.868",
+        cover:'/img/mobile.png',
+        type:"React + Typescript",
+        mark:'Design'
+    },
+    {
+        id: 5,
+        curse_name: "Inteligência Artificial",
+        description: "Explore as técnicas avançadas de inteligência artificial e aprenda a criar sistemas que podem aprender e tomar decisões de forma autônoma.",
+        localization: "Online",
+        student_number: "126.868",
+        cover:'/img/artificial.jpg',
+        type:"Python + Deep Learning",
+        mark:'Coding'
+    },
+    {
+        id: 6,
+        curse_name: "Segurança da Informação",
+        description: "Aprenda a proteger sistemas e redes contra ameaças cibernéticas, e a realizar testes de penetração para identificar vulnerabilidades.",
+        localization: "Presencial",
+        student_number: "12,356.868",
+        cover:'/img/cover.png',
+        type:"Linux + Windows",
+        mark:'Coding'
+    },
+    {
+        id: 7,
+        curse_name: "Machine Learning",
+        description: "Domine as técnicas de machine learning e aprenda a construir modelos preditivos e sistemas de recomendação.",
+        localization: "Online",
+        student_number: "156.868",
+        cover:'/img/machine.png',
+        type:"Python + Deep Learning",
+        mark:'Algorithm'
+    },
+    {
+        id: 8,
+        curse_name: "Blockchain e Criptomoedas",
+        description: "Explore a tecnologia blockchain e seu potencial para revolucionar setores como finanças, cadeia de suprimentos e muito mais.",
+        localization: "Presencial",
+        student_number: "868",
+        cover:'/img/blockchain.jpg',
+        type:"Python + C + C++",
+        mark:'Algorithm'
+    },
+    {
+        id: 9,
+        curse_name: "Desenvolvimento de Aplicativos Corporativos",
+        description: "Aprenda a criar aplicativos empresariais de alta qualidade para atender às necessidades específicas das organizações.",
+        localization: "Online",
+        student_number: "6.868",
+        cover:'/img/game.jpg',
+        type:"Python + Deep Learning",
+        mark:'Design'
+    },
+    {
+        id: 10,
+        curse_name: "Cloud Computing",
+        description: "Explore os fundamentos da computação em nuvem e aprenda a criar, implantar e gerenciar aplicativos escaláveis na nuvem.",
+        localization: "Presencial",
+        student_number: "12,356.868",
+        cover:'/img/cloud.jpg',
+        type:"Python + Deep Learning",
+        mark:'Algorithm'
+    },
+    {
+        id: 11,
+        curse_name: "Desenvolvimento de APIs",
+        description: "Aprenda a criar APIs robustas e escaláveis para facilitar a integração entre sistemas e aplicativos.",
+        localization: "Online",
+        student_number: "2,356.868",
+        cover:'/img/api.png',
+        type:"Nodejs + Express",
+        mark:'Coding'
+    },
+    {
+        id: 12,
+        curse_name: "Big Data",
+        description: "Domine as tecnologias e técnicas necessárias para lidar com grandes volumes de dados e extrair insights valiosos.",
+        localization: "Presencial",
+        student_number: "26.868",
+        cover:'/img/data.png',
+        type:"Python + Deep Learning",
+        mark:'Algorithm'
+    }
+];
+
